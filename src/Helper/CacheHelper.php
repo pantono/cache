039552789f043ea3f9570f34bfcef873734eb29d
Chris Burton <chris.burton@revolutionbarsgroup.com>
2025-08-09 20:10:06 +0100

    Better way of cleaning cache keys

--- src/Helper/CacheHelper.php
+++ src/Helper/CacheHelper.php
@@ -6,11 +6,13 @@ class CacheHelper
 {
     public static function cleanCacheKey(string $key): string
     {
-        $key = str_replace('/', '-', $key);
-        $key = str_replace('\\', '--', $key);
-        $key = str_replace('{', '', $key);
-        $key = str_replace('}', '', $key);
-        $key = str_replace('@', '-at-', $key);
-        return $key;
+        if (strlen($key) > 64) {
+            $key = substr($key, 0, 64);
+        }
+        $key = preg_replace('/[^A-Za-z0-9._]/', '_', $key);
+        if (preg_match('/^[0-9.]/', $key)) {
+            $key = '_' . $key;
+        }
+        return preg_replace('/_+/', '_', $key);
     }
 }
